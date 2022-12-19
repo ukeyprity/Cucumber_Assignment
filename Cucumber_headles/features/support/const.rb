96ed15a281efb770a13707e723f9679457c32ca1
Prity Ukey <119838901+ukeyprity@users.noreply.github.com>
2022-12-19 14:05:00 +0530

    Delete Cucumber_headles directory

--- Cucumber_headles/features/support/const.rb
+++
@@ -1,8 +0,0 @@
-require 'rubygems'
- require 'selenium-webdriver'
- require 'browserstack/local'
- BeforeAll do
-     $driver.get "http://Flipkart.com"
-     $driver.manage.window.maximize
-     $driver.manage.timeouts.implicit_wait = 3
- end
